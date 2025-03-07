da797d70c06baaf0be3ff7122eaffd1af27f7643
Brittany Halterman <halterma@gmail.com>
2025-03-07 08:44:16 -0500

    Render stars

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Star from './Star';
+
 const StarRating = () => {
   // Initialize a 'courseRating' state
 
   // Write a function that returns 5 Star components
+  const renderStars = () => {
+    let stars = [];
+    const maxRating = 5;
+    for (let i = 0; i < maxRating; i++) {
+      stars.push(<Star key={i} />);
+    }
+    return stars;
+  };
 
   // Write an event handler that updates the courseRating state.
   // Pass the function to a Star component via props
@@ -10,6 +20,7 @@ const StarRating = () => {
   return (
     <ul className="course--stars">
       {/* Render the Star components */}
+      {renderStars()}
     </ul>
   );
 };
